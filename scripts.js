9c7ed095281ccbb6b58309352efb0094b6cc4167
Charli <charlispilecki@gmail.com>
2020-11-06 00:00:37 -0600

    Added initial files for HTML and CSS. Created cards for the different pages that will be displayed, utilized Bootstrap.

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// When user hits "Start Quiz" button, they should start quiz and go to quiz cards
+// add click event for quiz questions selection
+// "Coding Quiz" in navbar should reset and take user to the first screen
+// create timer functionality
+// Navbar "High Score" should take user to the High Score page
+
+// When user starts quiz, they are taken to the first quiz question
+// timer starts
+// they should be able to click to select their answer 
+// when they hit "submit" they should be A)taken to the next q and B)gain points OR lose time
+
+    // if a user gets a question wrong, they lose 10 seconds from their time
+       
+    // if a user gets a question right, then they get 1 point
+    // and your time keeps ticking but does not otherwise change
+       
+    // the game is over when the timer gets to 0
+
+    // then the user can add their initials and their score gets recorded 
